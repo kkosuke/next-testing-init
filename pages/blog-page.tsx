b172c89c6e6cfce935092b7c38b6916ce7be3646
kkosuke <kkosuke007@gmail.com>
2023-03-18 20:40:16 +0900

    create blogAPIs

--- pages/blog-page.tsx
+++ pages/blog-page.tsx
@@ -1,13 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Layout } from '../components/Layout'
-import styles from '../styles/Home.module.css'
+import { getAllPostsData } from '../lib/fetch'
+import Post from '../components/Post'
+import { GetStaticProps } from 'next'
+import { POST } from '../types/Types'
 
-const BlogPage: React.FC = () => {
+interface STATICPROPS {
+  posts: POST[]
+}
+
+const BlogPage: React.FC<STATICPROPS> = ({ posts }) => {
   return (
     <Layout title="Blog">
-      <p className="text-4xl">blog page</p>
+      <p className="text-4xl mb-10">blog page</p>
+      <ul>{posts && posts.map((post) => <Post key={post.id} {...post} />)}</ul>
     </Layout>
   )
 }
 export default BlogPage
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getAllPostsData()
+  return {
+    props: { posts },
+  }
+}
